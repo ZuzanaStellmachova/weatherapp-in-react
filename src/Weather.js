10a4e1038f4d66d29f34e81fe7cb9cc1a2749a1c
ZuzanaStellmachova <113286338+ZuzanaStellmachova@users.noreply.github.com>
2023-02-05 11:46:10 +0100

    Forecast component

--- src/Weather.js
+++ src/Weather.js
@@ -2,8 +2,9 @@ import axios from "axios";
 import React, { useState } from "react";
 import Time from "./Time";
 import Day from "./CurrentDay";
-import Date from "./CurrentDate"
+import Date from "./CurrentDate";
 import CurrentTemperature from "./CurrentTemperature";
+import Forecast from "./Forecast"
 
 export default function Weather(props) {
     let [city, setCity] = useState(props.defaultCity);
@@ -103,117 +104,7 @@ export default function Weather(props) {
                     </div>
                     </div>
     
-                    <div className="forecast" id="forecast">
-                    <div className="day-forecast" id="day-forecast-0">
-                        <img src="icons/04n.svg" alt="" />
-                        <div className="temperature-wrapper">
-                        <div className="day-temperature-wrapper">
-                            <span className="day-temperature" id="day-temperature-0">
-                            18
-                            </span>
-                            °{" "}
-                        </div>
-                        |
-                        <div className="night-temperature-wrapper">
-                            <span className="night-temperature" id="night-temperature-0">
-                            8
-                            </span>
-                            °
-                        </div>
-                        </div>
-                        <div className="day-name" id="day-name-0">
-                        Mon
-                        </div>
-                    </div>
-    
-                    <div className="day-forecast" id="day-forecast-1">
-                        <img src="icons/04n.svg" alt="" />
-                        <div className="temperature-wrapper">
-                        <div className="day-temperature-wrapper">
-                            <span className="day-temperature" id="day-temperature-1">
-                            30
-                            </span>
-                            °{" "}
-                        </div>
-                        |
-                        <div className="night-temperature-wrapper">
-                            <span className="night-temperature" id="night-temperature-1">
-                            0
-                            </span>
-                            °
-                        </div>
-                        </div>
-                        <div className="day-name" id="day-name-1">
-                        Tue
-                        </div>
-                    </div>
-    
-                    <div className="day-forecast" id="day-forecast-2">
-                        <img src="icons/04n.svg" alt="" />
-                        <div className="temperature-wrapper">
-                        <div className="day-temperature-wrapper">
-                            <span className="day-temperature" id="day-temperature-2">
-                            29
-                            </span>
-                            °{" "}
-                        </div>
-                        |
-                        <div className="night-temperature-wrapper">
-                            <span className="night-temperature" id="night-temperature-2">
-                            9
-                            </span>
-                            °
-                        </div>
-                        </div>
-                        <div className="day-name" id="day-name-2">
-                        Wed
-                        </div>
-                    </div>
-    
-                    <div className="day-forecast" id="day-forecast-3">
-                        <img src="icons/04n.svg" alt="" />
-                        <div className="temperature-wrapper">
-                        <div className="day-temperature-wrapper">
-                            <span className="day-temperature" id="day-temperature-3">
-                            30
-                            </span>
-                            °
-                        </div>
-                        |
-                        <div className="night-temperature-wrapper">
-                            <span className="night-temperature" id="night-temperature-3">
-                            4
-                            </span>
-                            °
-                        </div>
-                        </div>
-                        <div className="day-name" id="day-name-3">
-                        Thu
-                        </div>
-                    </div>
-    
-                    <div className="day-forecast" id="day-forecast-4">
-                        <img src="icons/04n.svg" alt="" />
-                        <div className="temperature-wrapper">
-                        <div className="day-temperature-wrapper">
-                            <span className="day-temperature" id="day-temperature-4">
-                            19
-                            </span>
-                            °{" "}
-                        </div>
-                        |
-                        <div className="night-temperature-wrapper">
-                            <span className="night-temperature" id="night-temperature-4">
-                            14
-                            </span>
-                            °
-                        </div>
-                        </div>
-                        <div className="day-name" id="day-name-4">
-                        Fri
-                        </div>
-                    </div>
-                    </div>
+                    <Forecast />
                 </div>    
             </>
         )
